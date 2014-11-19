7797572485a2825afd0882f5e6b057e08ab8e2b4
knaopel <kurt@kurtopel.com>
2014-11-19 07:51:28 -0500

    Export Method Complete - Working on Import Method

--- SPNavigationImportExport.ps1
+++ SPNavigationImportExport.ps1
@@ -1,4 +1,125 @@
-﻿function Import-SPNavigation {
+﻿function New-TermsFromNavNodes {
+	param(
+	[Parameter(Mandatory = $true)]
+	[PSObject]$NavigationNodes,
+	[Parameter(Mandatory = $true)]
+	[Microsoft.SharePoint.Publishing.Navigation.NavigationTerm]$ParentTerm
+	)
+	$NavigationNodes | % {
+		$NavigationNode = $_
+		$newTerm = $ParentTerm.CreateTerm($NavigationNode.Title, [Microsoft.SharePoint.Publishing.Navigation.NavigationLinkType]::SimpleLink)
+		if($NavigationNode.NavigationNodes.Count -gt 0) {
+			New-TermsFromNavNodes -NavigationNodes $NavigationNode.NavigationNodes -ParentTerm $newTerm
+		}
+	}
+}
+function New-NavigationTermSet {
+	param(
+	[Parameter(Mandatory = $true)]
+	[PSObject]$NavigationSetNav,
+	[Parameter(Mandatory = $true)]
+	[string]$WebUrl,
+	[Parameter(Mandatory = $true)]
+	[string]$TermSetType
+	)
+	
+	$web = Get-SPWeb -Identity $WebUrl
+	
+	$taxSession = Get-SPTaxonomySession -Site $web.Site
+	$termStore = $taxSession.DefaultSiteCollectionTermStore
+	$siteCollectionGroup = $termStore.GetSiteCollectionGroup($web.Site)
+	
+	$termSet = $siteCollectionGroup.CreateTermSet($NavigationSetNav.Title.Value)
+	
+	if($TermSetType -eq "Current") {
+		$navProviderName = [Microsoft.SharePoint.Publishing.Navigation.StandardNavigationProviderNames]::CurrentNavigationTaxonomyProvider
+	} else {
+		$navProviderName = [Microsoft.SharePoint.Publishing.Navigation.StandardNavigationProviderNames]::GlobalNavigationTaxonomyProvider
+	}
+	
+	$navTermSet = [Microsoft.SharePoint.Publishing.Navigation.NavigationTermSet]::GetAsResolvedByWeb($termSet, $web, $navProviderName)
+	$navTermSet.IsNavigationTermSet = $true
+	
+	$NavigationSetNav.NavigationNodes | % {
+		$NavigationNode = $_
+		$NavigationTerm = $NavigationTermSet.CreateTerm($NavigationNode.Title, [Microsoft.SharePoint.Publishing.Navigation.NavigationLinkType]::SimpleLink)
+		if($NavigationNode.NavigationNodes.Count -gt 0) {
+			New-TermsFromNavNodes -NavigationNodes $NavigationNode.NavigationNodes -ParentTerm $NavigationTerm
+		}
+	}
+	
+	$termStore.CommitAll()
+	
+	$web.Dispose()
+	
+	return $navTermSet
+}
+
+function Add-WebNavigation {
+	param(
+	[Parameter(Mandatory = $true)]
+	[string]$SiteUrl,
+	[Parameter(Mandatory = $true)]
+	[PSObject]$NavigationSet
+	)
+	
+	$s = Get-SPSite -Identity $SiteUrl
+	$w = $s.AllWebs | ? { $_.ServerRelativeUrl -eq $NavigationSet.Url }
+	
+	$navSettings = New-Object -TypeName Microsoft.SharePoint.Publishing.Navigation.WebNavigationSettings -ArgumentList $w
+	$taxSession = Get-SPTaxonomySession -Site $s
+	# we might need to create a DefaultSiteCollectionTermStore first
+	$termStore = $taxSession.DefaultSiteCollectionTermStore
+	# we might need toi create SiteCollectionGroup if it doesn't exist
+	$termGroup = $termStore.GetSiteCollectionGroup($s)
+	
+	# reset navigation
+	$navSettings.ResetToDefaults();
+	if($NavigationSet.GlobalNavigation) {
+		# web should use global navigation
+		# create a new termstore
+		$globalTermSet = New-NavigationTermSet -WebUrl $w.Url -NavigationSetNav $NavigationSet.GlobalNavigation -TermSetType "Global"
+		$navSettings.GlobalNavigation.Source = [Microsoft.SharePoint.Publishing.Navigation.StandardNavigationSource]::TaxonomyProvider
+		$navSettings.GlobalNavigation.TermStoreId = $termStore.Id
+		$navSettings.GlobalNavigation.TermSetId = $globalTermSet.Id
+	}
+	
+	if($NavigationSet.CurrentNavigation) {
+		# web should use current navigation
+		$currentTermSet = New-NavigationTermSet -WebUrl $w.Url -NavigationSetNav $NavigationSet.CurrentNavigation -TermSetType "Current"
+		$navSettings.CurrentNavigation.Source = [Microsoft.SharePoint.Publishing.Navigation.StandardNavigationSource]::TaxonomyProvider
+		$navSettings.CurrentNavigation.TermStoreId = $termStore.Id
+		$navSettings.CurrentNavigation.TermSetId = $currentTermSet.Id
+	}
+	
+	$navSettings.Update($taxSession)
+	[Microsoft.SharePoint.Publishing.Navigation.TaxonomyNavigation]::FlushSiteFromCache($w)
+	
+	
+	
+	#$navGlobalTermSet = [Microsoft.SharePoint.Publishing.Navigation.TaxonomyNavigation]::GetTermSetForWeb($w, "GlobalNavigationTaxonomyProvider", $false)
+	#$navCurrentTermSet = [Microsoft.SharePoint.Publishing.Navigation.TaxonomyNavigation]::GetTermSetForWeb($w, "CurrentNavigationTaxonomyProvider", $false)	
+	# use $editableNavSet = $navGlobalTermSet.GetAsEditable($taxSession) # to get an editable version
+	
+	$w.Dispose()
+	$s.Dispose()
+}
+
+function Import-SPNavigation {
+	param(
+	[Parameter(Mandatory = $true)]
+	[string]$SiteUrl,
+	[Parameter(Mandatory = $true)]
+	[string]$InputXmlPath
+	)
+	
+	$pubAsm = [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint.Publishing");
+	
+	$NavigationSets = Import-Clixml -LiteralPath $InputXmlPath -Verbose
+	$NavigationSets | % {
+		$NavigationSet = $_
+		Add-WebNavigation -SiteUrl $SiteUrl -NavigationSet $NavigationSet
+	}
 }
 
 function Export-SPNavigation {
@@ -10,18 +131,30 @@ function Export-SPNavigation {
     )
     
     $pubAsm = [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint.Publishing");
-    $IsSP2010 = $pubAsm.FullName -match "Version=14"
-    
-    $site = Get-SPSite -Identity $SiteUrl
 
-    if ($IsSP2010) {
-    
-        $oSiteNav = Export-SPNavigationStructured -WebUrl $site.RootWeb.Url
-    } else {
+	$NavTermSets = @()
 
-    }
-    $site.Dispose()
-    $oSiteNav | Export-Clixml D:\Scripts\PowerShell\G2G.SharePoint.H2O\Navigation.xml
+	Get-SPWeb -Site $SiteUrl -Limit All | % {
+	    $web = $_
+	    $navSettings = New-Object -TypeName Microsoft.SharePoint.Publishing.Navigation.WebNavigationSettings -ArgumentList $web
+	    $site = $web.Site
+	    $taxSession = Get-SPTaxonomySession -Site $site
+	    $termStore = $taxSession.DefaultSiteCollectionTermStore
+	#    $globalTermSet = $termStore.GetTermSet($navSettings.GlobalNavigation.TermSetId)
+	    $navGlobalTermSet = [Microsoft.SharePoint.Publishing.Navigation.TaxonomyNavigation]::GetTermSetForWeb($web, "GlobalNavigationTaxonomyProvider", $false)
+		$navCurrentTermSet = [Microsoft.SharePoint.Publishing.Navigation.TaxonomyNavigation]::GetTermSetForWeb($web, "CurrentNavigationTaxonomyProvider", $false)	
+		$globalNav = Get-NavTermSetFromTaxonomy -NavigationTermSet $navGlobalTermSet	
+		$currentNav = Get-NavTermSetFromTaxonomy -NavigationTermSet $navCurrentTermSet
+		$webNavigation = New-Object PSObject
+		$webNavigation | Add-Member -MemberType NoteProperty -Name "Url" -Value $web.ServerRelativeUrl
+		$webNavigation | Add-Member -MemberType NoteProperty -Name "GlobalNavigation" -Value $globalNav
+		$webNavigation | Add-Member -MemberType NoteProperty -Name "CurrentNavigation" -Value $currentNav
+	    # use $editableNavSet = $navGlobalTermSet.GetAsEditable($taxSession) # to get an editable version
+		$NavTermSets += $webNavigation	
+		#ConvertTo-Xml -InputObject $webNavigation -Depth 8 -As String
+	}
+	Export-Clixml -Depth 9 -InputObject $NavTermSets -Path $OutputXmlPath
+#	ConvertTo-Xml -InputObject $NavTermSets -Depth 8 -As String
 }
 
 function Export-SPNavigationManaged {
@@ -109,15 +242,43 @@ function New-NavigationNode {
     return $navigationNode
 }
 
-$pubAsm = [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint.Publishing");
-
-Get-SPWeb -Identity "http://qa.livgov.com" | % {
-    $web = $_
-    $navSettings = New-Object -TypeName Microsoft.SharePoint.Publishing.Navigation.WebNavigationSettings -ArgumentList $web
-    $site = $web.Site
-    $taxSession = Get-SPTaxonomySession -Site $site
-    $termStore = $taxSession.DefaultSiteCollectionTermStore
-#    $globalTermSet = $termStore.GetTermSet($navSettings.GlobalNavigation.TermSetId)
-    $navGlobalTermSet = [Microsoft.SharePoint.Publishing.Navigation.TaxonomyNavigation]::GetTermSetForWeb($web, "GlobalNavigationTaxonomyProvider", $false)
-    # use $editableNavSet = $navGlobalTermSet.GetAsEditable($taxSession) # to get an editable version
+function Get-NavNodesFromTerms {
+	param(
+		[Parameter(Mandatory = $true)]
+    	$Terms
+	)
+	
+	$nodes = @()
+	
+	$Terms | % {
+		$Term = $_
+		$navNode = New-Object PSObject
+		$navNode | Add-Member -MemberType NoteProperty -Name "Title" -Value $Term.Title
+		$navNode | Add-Member -MemberType NoteProperty -Name "Url" -Value $Term.SimpleLinkUrl
+		$navNode | Add-Member -MemberType NoteProperty -Name "NavigationNodes" -Value (Get-NavNodesFromTerms -Terms $Term.Terms)
+		$nodes += $navNode
+	}
+	return $nodes
 }
+
+function Get-NavTermSetFromTaxonomy {
+	param(
+		[Parameter(Mandatory = $false)]
+    	[Microsoft.SharePoint.Publishing.Navigation.NavigationTermSet]$NavigationTermSet
+	)
+	
+	if($NavigationTermSet -eq $null) {
+		return $null
+	} else {
+		$navTermSet = New-Object PSObject
+		$navTermSet | Add-Member -MemberType NoteProperty -Name "Id" -Value $NavigationTermSet.Id
+		$navTermSet | Add-Member -MemberType NoteProperty -Name "Title" -Value $NavigationTermSet.Title
+		$navTermSet | Add-Member -MemberType NoteProperty -Name "NavigationNodes" -Value (Get-NavNodesFromTerms -Terms $NavigationTermSet.Terms)
+		
+		return $navTermSet
+	}
+}
+
+
+#Export-SPNavigation -SiteUrl "http://qa.livgov.com" -OutputXmlPath 'D:\Nav Backups\qa.livgov.com.xml'
+Import-SPNavigation -SiteUrl "http://qa2.livgov.com" -InputXmlPath 'D:\Nav Backups\qa.livgov.com.xml'
